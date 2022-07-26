dde98844a0fee1921204c22b9acef7e16da7b18c
unknown <jmcc986@gmail.com>
2022-07-26 15:26:12 -0400

    all homepages showing successfully

--- controllers/home_pages_controllers.js
+++ controllers/home_pages_controllers.js
@@ -1,3 +1,34 @@
+// Index Page
 exports.Index = async (req, res, next) => {
   res.render("pages/homepages/index");
 };
+
+// About Us Page
+exports.aboutUs = async (req, res, next) => {
+  res.render('pages/homepages/about_us')
+}
+
+// Apply Page
+exports.apply = async (req, res, next) => {
+  res.render('pages/homepages/apply')
+}
+
+// Apply Client
+exports.applyClient = async (req, res, next) => {
+  res.render('pages/homepages/apply_client')
+}
+
+// Apply Employee
+exports.applyEmployee = async (req, res, next) => {
+  res.render('pages/homepages/apply_employee')
+}
+
+// Contact Page
+exports.contact = async (req, res, next) => {
+  res.render('pages/homepages/contact')
+}
+
+// Login Page
+exports.login = async (req, res, next) => {
+  res.render('pages/homepages/login')
+}
--- routes/home_page_routes.js
+++ routes/home_page_routes.js
@@ -1,8 +1,14 @@
 const express = require("express");
 const router = express.Router();
 
-const { Index } = require("../controllers/home_pages_controllers");
+const { Index, aboutUs, apply, applyClient, applyEmployee, contact, login } = require("../controllers/home_pages_controllers");
 
 router.route("/").get(Index);
+router.route('/about-us').get(aboutUs)
+router.route('/apply').get(apply)
+router.route('/apply/client').get(applyClient)
+router.route('/apply/employee').get(applyEmployee)
+router.route('/contact').get(contact)
+router.route('/login').get(login)
 
 module.exports = router;
