1df55304b6282f5e87d0fa2cf0aa42788c5f3b65
unknown <jmcc986@gmail.com>
2022-07-28 12:42:09 -0400

    Client Form Schema added:

---
+++ models/ClientForm.js
@@ -0,0 +1,66 @@
+const mongoose = require('mongoose')
+
+const ClientFormSchema = new mongoose.Schema({
+    First: {
+        type: String,
+        required: true
+
+    },
+    Middle: {
+        type: String,
+
+
+    },
+    Last: {
+        type: String,
+        required: true,
+    },
+    DOB: {
+        type: Date,
+        required: true
+    },
+    Address: {
+        type: String,
+        required: true
+    },
+    Email: {
+        type: String,
+        match: [
+            /[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?/,
+          ],
+          required: True
+    },
+    HomePhone: {
+        type: String
+
+    },
+    MobilePhone: {
+        type: String
+    },
+    EmergencyContactName: {
+        type: String,
+        required: true
+    },
+    EmergencyContactRelationship: {
+        type: String,
+        required: true
+    },
+    EmergencyContactNumber: {
+        type: String,
+        required: true
+    },
+    PrimaryProvider: {
+        type: String,
+        required: true
+    },
+    SecondaryProvider: {
+        type: String,
+    },
+    createdAt: {
+        type: Date,
+        default: Date.now()
+    }
+
+})
+
+module.exports = mongoose.model("ClientForm", ClientFormSchema);
